6e43f151860ff4177c6e52333167acb3efe657d7
Chong Feng <chong.feng@koresoftware.com>
2019-09-22 22:16:18 +1000

    Seed sql script

--- SQL/Seed.sql
+++ SQL/Seed.sql
@@ -1,3 +1,6 @@
+/*
+Create database for catalog api
+*/
 CREATE DATABASE CatalogDb
 GO
 
@@ -6,10 +9,40 @@ GO
 
 CREATE TABLE CatalogBrands 
 (
-	Id int PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	Id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
 	Brand nvarchar(50)
 )
 GO
 
+
+CREATE TABLE CatalogTypes
+(
+	Id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	Type nvarchar(50)
+)
+GO
+
+
+CREATE TABLE CatalogItems
+(
+	Id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	Name NVARCHAR(100),
+	Description NVARCHAR(1000),
+	Price DECIMAL(19,4),
+	PictureUri NVARCHAR(100),
+	CatalogTypeId INT NOT NULL FOREIGN KEY REFERENCES CatalogType(Id),
+	CatalogBrandId INT NOT NULL FOREIGN KEY REFERENCES CatalogBrands(Id),
+	AvailableStock INT,
+	RestockThreshold INT,
+	MaxStockThreshold INT,
+	OnReorder BIT
+)
+
 INSERT INTO CatalogBrands (Brand) VALUES ('BMW')
 GO
+
+
+/*
+Create database for integration event log
+*/
+CREATE DATABASE 
